3d0a55c6ddd00bb5adea25013a80e9d2d65b1bc0
baay02 <baaybilya25@yahoo.com>
2023-05-09 15:53:47 -0700

    advance task3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,24 +1,18 @@
+#!/usr/bin/python3
+""" recursive function that queries the Reddit API"""
 import requests
+import sys
+after = None
+count_dic = []
 
-def count_words(subreddit, word_list, after=None, counts={}):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()["data"]["children"]
-        for post in data:
-            title = post["data"]["title"].lower()
-            for word in word_list:
-                word = word.lower()
-                if f" {word} " in f" {title} ":
-                    counts[word] = counts.get(word, 0) + 1
-        after = response.json()["data"]["after"]
-        if after:
-            return count_words(subreddit, word_list, after, counts)
-        else:
-            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-            for count in sorted_counts:
-                print(f"{count[0]}: {count[1]}")
-    else:
-        print(None)
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
